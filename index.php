fa305e8190d850d724c4ac7ff879b80acb449d11
Geraldine743 <egeraldine2@gmail.com>
2025-06-02 17:12:12 +0200

    add home page

---
+++ index.php
@@ -0,0 +1,41 @@
+<?php
+require_once __DIR__ . "/templates/header.php";
+
+$articles = [
+    ["title" => "PHP VS Python", "content" => "Dans cet article, nous allons explorer les différences et les avantages de PHP et Python, deux langages de programmation populaires, afin de vous aider à choisir celui qui convient le mieux à vos projets.", "image" => "1-php-vs-python.jpg"],
+    ["title" => "react VS react native", "content" => "Dans cet article, nous allons explorer les différences clés entre React et React Native, afin de mieux comprendre leurs spécificités et leurs applications respectives dans le développement d'applications web et mobiles.", "image" => "2-react-vs-react-native.jpg"],
+    ["title" => "Les meilleurs outils de devops", "content" => "Découvrez les meilleurs outils DevOps qui optimisent la collaboration entre les équipes de développement et d'exploitation, améliorent l'automatisation des processus et garantissent une livraison continue de qualité.", "image" => "3-devops.png"]
+];
+
+?>
+
+<div class="row flex-lg-row-reverse align-items-center g-5 py-5">
+    <div class="col-10 col-sm-8 col-lg-6">
+        <img src="assets/image/logo-tech-trendz.png" class="d-block mx-lg-auto img-fluid" alt="logo techtrendz" width="700" height="500" loading="lazy">
+    </div>
+    <div class="col-lg-6">
+        <h1 class="display-5 fw-bold text-body-emphasis lh-1 mb-3">Explorez le futur, un pixel à la fois !</h1>
+        <p class="lead">Découvrez notre blog dédié à l'informatique, où passion et expertise se rencontrent pour vous offrir des articles variés sur les dernières tendances, astuces et innovations technologiques.</p>
+        <div class="d-grid gap-2 d-md-flex justify-content-md-start">
+            <a href="#" class="btn btn-primary btn-lg px-4 me-md-2">Voir les actualités</a>
+        </div>
+    </div>
+</div>
+
+<div class="row text-center">
+    <?php foreach ($articles as $key => $article) { ?>
+        <div class="col-md-4 my-2">
+            <div class="card">
+                <img src="./uploads/<?= $article["image"] ?>" class="card-img-top" alt="<?= $article["image"] ?>">
+                <div class="card-body">
+                    <h5 class="card-title"><?= $article["title"] ?></h5>
+                    <p class="card-text"><?= $article["content"] ?></p>
+                    <a href="#" class="btn btn-primary">Voir l'article</a>
+                </div>
+            </div>
+        </div>
+    <?php } ?>
+</div>
+
+
+<?php require_once __DIR__ . "/templates/footer.php" ?>
---
+++ templates/header.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/css/override-bootstrap.css">
+    <title>TechTrendz</title>
+</head>
+
+<body>
+    <div class="container">
+        <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
+            <div class="col-md-3 mb-2 mb-md-0">
+                <a href="/" class="d-inline-flex link-body-emphasis text-decoration-none">
+                    <img src="assets/image/logo-tech-trendz.png" alt="logo" width="150">
+                </a>
+            </div>
+            <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+                <li><a href="#" class="nav-link px-2 link-secondary">Accueil</a></li>
+                <li><a href="#" class="nav-link px-2">Actualité</a></li>
+                <li><a href="#" class="nav-link px-2">A propos</a></li>
+            </ul>
+            <div class="col-md-3 text-end">
+                <button type="button" class="btn btn-outline-primary me-2">Se connecter</button>
+                <button type="button" class="btn btn-primary">S'inscrire</button>
+            </div>
+        </header>
+        <main>
