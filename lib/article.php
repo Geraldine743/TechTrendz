c3a676e12ed9dd369342dda82132f13df1e74f25
Geraldine743 <egeraldine2@gmail.com>
2025-06-04 17:09:55 +0200

    add function getArticleById() and add function getArticleImage()

--- lib/article.php
+++ lib/article.php
@@ -13,3 +13,21 @@ function getArticles(PDO $pdo, ?int $limit = null): array
     $query->execute();
     return $query->fetchAll(PDO::FETCH_ASSOC);
 }
+
+function getArticleById(PDO $pdo, int $id): array|bool
+{
+    $sql = "SELECT * FROM articles WHERE id = :id";
+    $query = $pdo->prepare($sql);
+    $query->bindValue(":id", $id, PDO::PARAM_INT);
+    $query->execute();
+    return $query->fetch(PDO::FETCH_ASSOC);
+}
+
+function getArticleImage(string|null $image): string
+{
+    if ($image === null) {
+        return _ASSETS_IMAGES_FOLDER_ . "default-article.jpg";
+    } else {
+        return _ARTICLES_IMAGES_FOLDER_ . $image;
+    }
+}
--- templates/article_part.php
+++ templates/article_part.php
@@ -1,23 +1,17 @@
 <?php
 
-if ($article["image"] === null) {
-    $imagePath = _ASSETS_IMAGES_FOLDER_ . "default-article.jpg";
-    $imageAlt = "image par defaut";
-} else {
-    $imagePath = _ARTICLES_IMAGES_FOLDER_ . $article["image"];
-    $imageAlt = $article["image"];
-}
+$imagePath = getArticleImage($article["image"]);
 
 ?>
 
 
 <div class="col-md-4 my-2">
     <div class="card">
-        <img src="<?= $imagePath ?>" class="card-img-top" alt="<?= $imageAlt ?>">
+        <img src="<?= $imagePath ?>" class="card-img-top" alt="<?= $article["title"] ?>">
         <div class="card-body">
             <h5 class="card-title"><?= $article["title"] ?></h5>
             <p class="card-text"><?= $article["content"] ?></p>
-            <a href="actualite.php?id=<?= $key ?>" class="btn btn-primary">Voir l'article</a>
+            <a href="actualite.php?id=<?= $article["id"] ?>" class="btn btn-primary">Voir l'article</a>
         </div>
     </div>
 </div>
