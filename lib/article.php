7b68b612f58520e695bbb4d429c6fe234e72a5d6
Geraldine743 <egeraldine2@gmail.com>
2025-06-04 16:10:45 +0200

    add function getArticles ()

--- lib/article.php
+++ lib/article.php
@@ -1,7 +1,15 @@
 <?php
 
-$articles = [
-    ["title" => "PHP VS Python", "content" => "Dans cet article, nous allons explorer les différences et les avantages de PHP et Python, deux langages de programmation populaires, afin de vous aider à choisir celui qui convient le mieux à vos projets.", "image" => "1-php-vs-python.jpg"],
-    ["title" => "react VS react native", "content" => "Dans cet article, nous allons explorer les différences clés entre React et React Native, afin de mieux comprendre leurs spécificités et leurs applications respectives dans le développement d'applications web et mobiles.", "image" => "2-react-vs-react-native.jpg"],
-    ["title" => "Les meilleurs outils de devops", "content" => "Découvrez les meilleurs outils DevOps qui optimisent la collaboration entre les équipes de développement et d'exploitation, améliorent l'automatisation des processus et garantissent une livraison continue de qualité.", "image" => "3-devops.png"]
-];
+function getArticles(PDO $pdo, ?int $limit = null): array
+{
+    $sql = "SELECT * FROM articles ORDER BY id DESC";
+    if ($limit) {
+        $sql .= " LIMIT :limit";
+    }
+    $query = $pdo->prepare($sql);
+    if ($limit) {
+        $query->bindValue(":limit", $limit, PDO::PARAM_INT);
+    }
+    $query->execute();
+    return $query->fetchAll(PDO::FETCH_ASSOC);
+}
--- templates/article_part.php
+++ templates/article_part.php
@@ -1,6 +1,19 @@
+<?php
+
+if ($article["image"] === null) {
+    $imagePath = _ASSETS_IMAGES_FOLDER_ . "default-article.jpg";
+    $imageAlt = "image par defaut";
+} else {
+    $imagePath = _ARTICLES_IMAGES_FOLDER_ . $article["image"];
+    $imageAlt = $article["image"];
+}
+
+?>
+
+
 <div class="col-md-4 my-2">
     <div class="card">
-        <img src="./uploads/<?= $article["image"] ?>" class="card-img-top" alt="<?= $article["image"] ?>">
+        <img src="<?= $imagePath ?>" class="card-img-top" alt="<?= $imageAlt ?>">
         <div class="card-body">
             <h5 class="card-title"><?= $article["title"] ?></h5>
             <p class="card-text"><?= $article["content"] ?></p>
