e112c214e9452daf7831f119a13ecad5be9062aa
Geraldine743 <egeraldine2@gmail.com>
2025-06-05 13:36:18 +0200

    add function adminOnly()

--- admin/templates/header.php
+++ admin/templates/header.php
@@ -1,3 +1,8 @@
+<?php
+require_once __DIR__ . "/../../lib/session.php";
+adminOnly();
+?>
+
 <!DOCTYPE html>
 <html lang="fr">
 
@@ -5,7 +10,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Administration</title>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
 
--- lib/session.php
+++ lib/session.php
@@ -1,3 +1,12 @@
 <?php
 
 session_start();
+
+function adminOnly()
+{
+    if (!isset($_SESSION['user'])) {
+        header('location: ../login.php');
+    } else if ($_SESSION['user']['role'] != 'admin') {
+        header('location: ../index.php');
+    }
+}
